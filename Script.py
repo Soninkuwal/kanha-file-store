d807ca5dee2cc2117135e13b6bc5dc95920ed054
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-23 14:33:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@
 
 
 class script(object):
-    START_TXT = """<b>😊 【HELLO BABY】 😍 {} 【I AM KANHA FILE STORAGE BOT💕❤️ MY FATHER❤️💕 @Chatkanhabot JOIN MY UPDATE CHANNEL👇👇👇👇👇👇👇👇 https://t.me/Sonickuwalupdate】 </b>"""
+    START_TXT = """<b>😊 【HELLO BABY】 😍 /n/n {} /n/n【I AM KANHA FILE STORAGE BOT /n/n💕❤️ MY FATHER❤️💕 @Chatkanhabot /n/nJOIN MY UPDATE CHANNEL👇👇👇👇👇👇👇👇/n/n https://t.me/Sonickuwalupdate】 </b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
