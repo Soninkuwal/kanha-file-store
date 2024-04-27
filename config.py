418e4ff1513562d69f455418e1afecdf6c48e5d6
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-04-27 17:47:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "kanha-file-store-f408b5429b35.herokuapp.com/"
+        URL = "chemical-termite-sonickuwal147rr.koyeb.app/"
     else:
-        URL = "kanha-file-store-f408b5429b35.herokuapp.com/"
+        URL = "chemical-termite-sonickuwal147rr.koyeb.app/"
 
 
 
